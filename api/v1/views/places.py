9fb59c8f7188ceefe9baf56e86a3a10390e47e48
zack6yuan <zack6yuan@gmail.com>
2024-11-10 10:26:46 -0600

    place prep.

---
+++ api/v1/views/places.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Place Module """
+from api.v1.views import app_views
+from flask import Flask
+from models.place import Place
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+def place_list():
+    """ Method: Retrieve list of all place objects of a City """
+
+
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def place_object():
+    """ Method: Retrieve an place object """
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+def place_delete():
+    """ Method: Delets a place object """
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+def place_create():
+    """ Method: Create an place object """
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def place_update():
+    """ Method: Update an place object """
