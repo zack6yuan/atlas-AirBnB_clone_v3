bca27cd93341d1f1b3dfddbe4dbe53535e27d734
zack6yuan <zack6yuan@gmail.com>
2024-11-10 10:45:57 -0600

    review prep.

--- api/v1/views/reviews.py
+++ api/v1/views/reviews.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import Flask
 from models.review import Review
+from models.place import Place
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
