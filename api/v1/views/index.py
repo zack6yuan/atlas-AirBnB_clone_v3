76909be6f9b5064587f5ebad02a8e58d8bfbb7ae
zack6yuan <zack6yuan@gmail.com>
2024-11-10 10:32:55 -0600

    update imports.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Method: Return JSON """
-from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask import Flask, jsonify
 
 @app_views.route('/status', strict_slashes=False)
 def return_json():
