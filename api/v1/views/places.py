07b489d74d96748987cc6935cccb273b374ddc80
zack6yuan <zack6yuan@gmail.com>
2024-11-10 10:45:49 -0600

    place prep.

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import Flask
 from models.place import Place
+from models.city import City
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
