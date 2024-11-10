4e86770a9e94b76bc75d65a37563918eac3227a4
zack6yuan <zack6yuan@gmail.com>
2024-11-10 09:24:19 -0600

    update index.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Method: Return JSON """
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def return_json():
-    
+    views = {"status": "OK"}
+    return (jsonify(views))
