66308e7edceb2a5611b6391ed2181fe689e0ed05
zack6yuan <zack6yuan@gmail.com>
2024-11-10 10:10:56 -0600

    cities prep.

---
+++ api/v1/views/cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" City Module """
+from api.v1.views import app_views
+from flask import Flask
+from models.city import City
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashses=False)
+def city_list():
+    """ Method: Retrieve list of all city objects of a state """
+
+
+@app_views.route('cities/<city_id>', methods=['GET'], strict_slashes=False)
+def city_object():
+    """ Method: Retrieve a city object """
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+def city_delete():
+    """ Method: Delets a city object """
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+def city_create():
+    """ Method: Create a city object """
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def city_update():
+    """ Method: Update a city object """
