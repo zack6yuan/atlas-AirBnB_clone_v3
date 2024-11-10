7b8cc0baeb4dfa1e289e597c468c2488c91b3fce
maxnchief <logandogan@gmail.com>
2024-11-10 14:06:48 -0600

    added stats endpoint to return counts of various models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,20 @@
 """ Method: Return JSON """
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def return_json():
     return (jsonify({"status": "OK"}))
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
