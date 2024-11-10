dfb94d95c3e2541d93abce9def93e1eb92de5a28
zack6yuan <zack6yuan@gmail.com>
2024-11-10 10:20:07 -0600

    state prep update function names for consistency.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,20 +6,20 @@ from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_state():
+def state_list():
     """ Method: Retrieve list of all State objects """
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def get_state_id():
+def state_object():
     """ Method: Retrieve a State object """
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state():
+def state_create():
     """ Method: Create a State """
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_state():
+def state_update():
     """ Method: Update State object """
