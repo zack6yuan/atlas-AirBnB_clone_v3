723fad484f71cd2e7d6079365f6edc63e36c257e
zack6yuan <zack6yuan@gmail.com>
2024-11-10 20:32:59 -0600

    updating state.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,24 +1,49 @@
 #!/usr/bin/python3
 """ State Module """
 from api.v1.views import app_views
-from flask import Flask
+from flask import jsonify, abort, request
 from models.state import State
+from models import storage
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def state_list():
     """ Method: Retrieve list of all State objects """
+    states = []
+    for state in states:
+        states.append(state.to_dict())
+    return jsonify(states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def state_get(state_id):
+    """ Method: Retrieve a State object """
+    obj = storage.all(State).values()
+    if obj is not None:
+        return jsonify(obj.to_dict())
+    else:
+        abort(404)
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def state_object():
-    """ Method: Retrieve a State object """
+def state_object(state_id):
+    """ Method: Delete a State object """
+    dict = {}
+    obj = storage.get(State, state_id)
+    if obj is not None:
+        storage.delete(obj)
+        storage.save()
+    if obj is None:
+        abort(404)
+    else:
+        return(dict)
+    
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def state_create():
     """ Method: Create a State """
-
+    
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def state_update():
