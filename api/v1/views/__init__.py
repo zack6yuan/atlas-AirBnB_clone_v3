4f5090cf4a37422541306314e84182f935aa538b
zack6yuan <zack6yuan@gmail.com>
2024-11-12 16:43:03 -0600

    update __init__.py.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,6 +10,5 @@ from api.v1.views.cities import *
 from api.v1.views.index import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
