a3e14c54334fdcf51f04f3745c409652b46333bb
zack6yuan <zack6yuan@gmail.com>
2024-11-12 00:21:37 -0600

    revert.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,10 +74,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-        
-    def get(self, cls, id):
-        """ Method: Retrieve one object """
-        
-        
-    def count(self, cls=None):
-        """ Method: Count objects in storage """
