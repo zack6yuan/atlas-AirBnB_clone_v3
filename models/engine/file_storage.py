a43341eaf9089e854957d9768f28cae72d148a02
maxnchief <logandogan@gmail.com>
2024-11-12 17:44:28 -0600

    fix: streamline JSON loading in FileStorage for improved error handling

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,10 +52,9 @@ class FileStorage:
         """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
-                content = f.read()
-                if content:
-                    jo = json.loads(content)
-                else:
+                try:
+                    jo = json.load(f)
+                except json.JSONDecodeError:
                     return
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
