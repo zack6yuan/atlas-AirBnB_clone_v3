6a2fbc68ea520b4bdefad11264aecfb1ae88f614
zack6yuan <zack6yuan@gmail.com>
2024-11-12 11:28:24 -0600

    whitespace.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,7 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/vi")
 
-from api.v1.v iews.amenities import *
+from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
 from api.v1.views.places import *
