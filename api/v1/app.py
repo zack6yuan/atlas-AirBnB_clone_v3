615c94e04ee873ebc806907f6c27f1e7ca4935b1
zack6yuan <zack6yuan@gmail.com>
2024-11-12 23:44:06 +0000

    update syntax.

--- api/v1/app.py
+++ api/v1/app.py
@@ -48,4 +48,4 @@ if __name__ == "__main__":
     """ Method: Set host and port for usage """
     host = os.getenv("HBNB_API_HOST", default="0.0.0.0")
     port = os.getenv("HBNB_API_PORT", default=5000)
-    app.run(host, port, threaded=True)
+    app.run(host=host, port=port, threaded=True)
