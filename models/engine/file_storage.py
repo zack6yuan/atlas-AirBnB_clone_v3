fdeccd94965d21edb9764bb81b71d6728766c1ec
maxnchief <logandogan@gmail.com>
2024-11-12 15:59:01 -0600

    PEP8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,7 +68,7 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-        
+
     def get(self, cls, id):
         """retrieves one object based on class and ID"""
         if cls is not None and id is not None:
@@ -82,4 +82,3 @@ class FileStorage:
         if cls is not None:
             return len(self.all(cls))
         return len(self.__objects)
-    
