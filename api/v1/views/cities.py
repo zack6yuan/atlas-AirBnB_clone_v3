45cef0f55d0dbc31942a1d8857c1c8c7263ad921
zack6yuan <zack6yuan@gmail.com>
2024-11-12 11:12:56 -0600

    spelling error.

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,7 +7,7 @@ from models.state import State
 from models import storage
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashses=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def city_list(state_id):
     """ Method: Retrieve list of all city objects of a state """
     obj = storage.get(State, state_id)
