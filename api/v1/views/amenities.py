bd65cf2bdc146144eb24ccb53d7f4537ded413cc
maxnchief <logandogan@gmail.com>
2024-11-12 14:39:48 -0600

    PEP8 fixes in amenitites

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -15,7 +15,8 @@ def amenity_list():
     return jsonify(amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def amenity_object(amenity_id):
     """ Method: Retrieve an amenity object """
     obj = storage.all(Amenity).values()
@@ -25,7 +26,8 @@ def amenity_object(amenity_id):
         abort(404)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def amenity_delete(amenity_id):
     """ Method: Delete an amenity object """
     obj = storage.get(Amenity, amenity_id)
@@ -35,7 +37,7 @@ def amenity_delete(amenity_id):
     if obj is None:
         abort(404)
     return jsonify({}), 200
-    
+
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def amenity_create():
@@ -50,7 +52,8 @@ def amenity_create():
         return (amenity_data)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def amenity_update(amenity_id):
     """ Method: Update an amenity  object """
     obj = storage.get(Amenity, amenity_id)
@@ -58,4 +61,3 @@ def amenity_update(amenity_id):
         abort(404)
     elif not request.is__json():
         abort(400, "Not a JSON")
-            
