0b654afd23993c172b279649080ecebd6df69162
zack6yuan <zack6yuan@gmail.com>
2024-11-12 11:11:55 -0600

    update index, imports + stats.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 
 @app_views.route('/status', strict_slashes=False)
 def return_json():
@@ -14,6 +20,6 @@ def get_stats():
     stats = {'states': State, 'users': User,
             'amenities': Amenity, 'cities': City,
             'places': Place, 'reviews': Review}
-    for key in todos:
+    for key in stats:
         stats[key] = storage.count(stats[key])
     return jsonify(stats)
