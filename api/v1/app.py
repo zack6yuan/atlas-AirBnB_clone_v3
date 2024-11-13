6ba8868af3c2a439850b0a7b6679037f9139f6ec
maxnchief <logandogan@gmail.com>
2024-11-13 14:05:18 -0600

    forcing app to run on 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,56 +1,26 @@
 #!/usr/bin/python3
-"""
-App Module
-
-This module sets up a Flask web application for the AirBnB clone project.
-It registers the application blueprint,
-configures the teardown context,
-and sets up error handling
-for 404 errors.
-
-Functions:
-    teardown(): Removes the current session.
-    not_found(error): Returns a JSON-formatted 404 status code response.
-
-Usage:
-    The application can be run directly,
-    and it will use the host and port specified
-    in the environment variables HBNB_API_HOST and HBNB_API_PORT,
-    or default to "0.0.0.0" and 5000 respectively.
-"""
-
-import os
+""" Main API module """
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-
-
 app.register_blueprint(app_views)
 
-""" Create CORS Instance"""
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """ Method: Remove the current session """
+def teardown_db(exception):
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """ Handler for 404 errors that returns a
-        JSON-formatted 404 status code response """
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    """ Method: Set host and port for usage """
-    from os import getenv
-    host = getenv('HBNB_API_HOST', "0.0.0.0")
-    port = getenv('HBNB_API_PORT', "5000")
-    app.run(host=host, port=port, threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
