741cb98a5a51e84223cb0d9eadbe2760bf86ccb8
maxnchief <logandogan@gmail.com>
2024-11-13 13:54:23 -0600

    fixing the host port setup

--- api/v1/app.py
+++ api/v1/app.py
@@ -50,6 +50,7 @@ def not_found(error):
 
 if __name__ == "__main__":
     """ Method: Set host and port for usage """
-    host = os.getenv("HBNB_API_HOST", default="0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", default=5000)
+    from os import getenv
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
