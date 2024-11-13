d8b29d7bda9128d0b9439accb5d78cf6f62103f2
zack6yuan <zack6yuan@gmail.com>
2024-11-13 00:43:25 +0000

    pushed to other branch, testing.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ Method: Return JSON """
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
+from models import storage
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 
-@app_views.route('/status', strict_slashes=False)
+
+@app_views.route("/status", strict_slashes=False)
 def return_json():
     return (jsonify({"status": "OK"}))
+
+
+@app_views.route("/stats", strict_slashes=False)
+def get_stats():
+    '''JSON Responses'''
+    stats = {"states": State, "users": User,
+             "amenities": Amenity, "cities": City,
+             "places": Place, "reviews": Review}
+    for key in stats:
+        stats[key] = storage.count(stats[key])
+    return jsonify(stats)
