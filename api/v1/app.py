f07fcb5478c3565399e40e058d4127e97d8a6212
zack6yuan <zack6yuan@gmail.com>
2024-11-13 01:57:00 +0000

    update.

--- api/v1/app.py
+++ api/v1/app.py
@@ -32,11 +32,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 """ Create CORS Instance: requests from /api/v1"""
-CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exception):
     """ Method: Remove the current session """
     storage.close()
 
