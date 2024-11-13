8cbb049d40ad5fbfbfd9e2529c8aa5ea8fdc9889
maxnchief <logandogan@gmail.com>
2024-11-12 20:30:20 -0600

    fix: update status route to use lowercase GET method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ from models.place import Place
 from models.review import Review
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['get'])
 def status():
     ''' routes to status page '''
     return jsonify({'status': 'OK'})
